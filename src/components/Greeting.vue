6a4a284e1bcef61b3d015e0123a9efe1b2231df9
rabbithawk256 <34002118+rabbithawk256@users.noreply.github.com>
2021-08-12 16:26:52 +0100

    Initial commit

---
+++ src/components/Greeting.vue
@@ -0,0 +1,20 @@
+<template>
+  <span>test</span>
+</template>
+
+<script>
+// DON'T DELETE!!!!!!!
+// it'll save future you from
+// so much headaches, wondering
+// why your code isn't working
+
+export default {
+  name: "Greeting",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "../";
+</style>
